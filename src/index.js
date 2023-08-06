8e3dbfc638fa61125b8bcf771895c69cd6bc1de4
Mina <smyrnahani@gmail.com>
2023-08-06 17:20:20 +0300

    index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DarkModeContextProvider } from './context/darkModeContext';
 import { Provider } from 'react-redux';
-import { store } from './redux/store'
+import { store } from './redux/Store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
